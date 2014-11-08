c266b5a1af3c501ac28068e08167d8ccf4aad37c
Daryl Chang <darylc123@gmail.com>
2014-11-08 13:57:38 -0800

    Refactored classify.py

--- classify.py
+++ classify.py
@@ -8,48 +8,53 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC, LinearSVC
 
-# Create feature and label vectors
-f = FeatureGen()
-X, Y = f.getXY()
-
 # Analyze mean force of PD and non-PD subjects
-# a = zip(X, Y)
-# nonPD = [x[1] for x, y in a if y == 0]
-# PD = [x[1] for x, y in a if y == 1]
-# nonPDmean, PDmean = np.mean(nonPD), np.mean(PD) 
-# nonPDvar, PDvar = np.var(nonPD), np.var(PD)
-# print "Mean: {}, variance: {}".format(nonPDmean, nonPDvar)
-# print "Mean: {}, variance: {}".format(PDmean, PDvar)
+def analyzeMeans(X, Y):
+	a = zip(X, Y)
+	nonPD = [x[1] for x, y in a if y == 0]
+	PD = [x[1] for x, y in a if y == 1]
+	nonPDmean, PDmean = np.mean(nonPD), np.mean(PD) 
+	nonPDvar, PDvar = np.var(nonPD), np.var(PD)
+	print "Mean: {}, variance: {}".format(nonPDmean, nonPDvar)
+	print "Mean: {}, variance: {}".format(PDmean, PDvar)
 
 # Run Logistic Regression and plot train and test errors
-trainingSizes = range(100, 250, 25)
-trainErrors, testErrors = [], []
-for i in trainingSizes:
-	trainX, testX = X[:i], X[i:]
-	trainY, testY = Y[:i], Y[i:]
-	clf = LogisticRegression()
-	clf.fit(trainX, trainY)
-	
-	# Training error
-	output = clf.predict(trainX)
-	numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, trainY)])
-	numTotal = float(len(trainY))
-	trainErrors.append(numWrong/numTotal)
+def plotTrainTest(clf, X, Y):
+	trainingSizes = range(100, 250, 25)
+	trainErrors, testErrors = [], []
+	for i in trainingSizes:
+		trainX, testX = X[:i], X[i:]
+		trainY, testY = Y[:i], Y[i:]
+		clf.fit(trainX, trainY)
+		
+		# Training error
+		output = clf.predict(trainX)
+		numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, trainY)])
+		numTotal = float(len(trainY))
+		trainErrors.append(numWrong/numTotal)
 
-	# Test error
-	output = clf.predict(testX)
-	numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, testY)])
-	numTotal = float(len(testY))
-	testErrors.append(numWrong/numTotal)
+		# Test error
+		output = clf.predict(testX)
+		numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, testY)])
+		numTotal = float(len(testY))
+		testErrors.append(numWrong/numTotal)
 
-pyplot.plot(trainingSizes, trainErrors, label="Training error")
-pyplot.plot(trainingSizes, testErrors, label="Test error")
-pyplot.legend()
-pyplot.xlabel('Training set size')
-pyplot.ylabel('Error')
-pyplot.title('Logistic regression train and test error vs. training set size')
-pyplot.show()
+	pyplot.plot(trainingSizes, trainErrors, label="Training error")
+	pyplot.plot(trainingSizes, testErrors, label="Test error")
+	pyplot.legend()
+	pyplot.xlabel('Training set size')
+	pyplot.ylabel('Error')
+	pyplot.title('{} train and test error vs. training set size'.format(clf.__class__.__name__))
+	pyplot.show()
 
+def main():
+	# Create feature and label vectors
+	f = FeatureGen()
+	X, Y = f.getXY()
+	analyzeMeans(X, Y)
+	plotTrainTest(LogisticRegression(), X, Y)
+
+main()
 
 # scores = cross_val_score(clf, X, Y, cv=5)
 # print "Test accuracy for Random Forest: {}".format(sum(scores)/len(scores))
