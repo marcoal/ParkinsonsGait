ea01db4807fd04e83e644539679693418afa8fcd
Marco <marco93alban@hotmail.com>
2014-11-10 15:16:59 -0800

    Implemented Forward Search for feature selection

---
+++ featureSelect.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+from sklearn.cross_validation import cross_val_score
+
+
+# This code wa adapted from Sebastian Raschka http://sebastianraschka.com/Articles/2014_sequential_sel_algos.html#sfs
+def forward_search(features, max_k, criterion_func, params):
+    '''
+    Forward search for best subset of features
+    :param features: list of indeces indicating features (i.e. list(xrange(
+    :param max_k:
+    :param criterion_func:
+    :param params:
+    :return:
+    '''
+
+    clf = params[0]
+    X = params[1]
+    Y = params[2]
+
+
+    # Initialize empty feature subset
+    feat_sub = []
+    k = 0
+    d = len(features)
+    if max_k > d:
+        max_k = d
+
+    while True:
+        # Evaluate criteriafunc on the subset of parameters
+        crit_func_max = criterion_func(feat_sub + [features[0]], clf, X, Y)
+        best_feat = features[0]
+        for x in features[1:]:
+            crit_func_eval = criterion_func(feat_sub + [x], clf, X, Y)
+            if crit_func_eval > crit_func_max:
+                crit_func_max = crit_func_eval
+                best_feat = x
+        feat_sub.append(best_feat)
+        features.remove(best_feat)
+
+        # Termination condition
+        k = len(feat_sub)
+        if k == max_k:
+            break
+
+    return feat_sub
+
+
+#Cross_validate accuracy on subset of features indicated by indexFeatures
+def cv_accuracy(indexFeatures, clf, X, Y):
+    X = np.array(X)
+    Y = np.array(Y)
+    scores = cross_val_score(clf, X[:, indexFeatures], Y, cv=5)
+    accuracy = sum(scores) / len(scores)
+    return accuracy
