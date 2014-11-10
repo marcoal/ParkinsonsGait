-Classify.py - Fixed space/tabs intermingled -featureGen.py - Added wavelet approx coefficient features (low-passed filtered signal), for l1 sensor cropped to 2000 because subjects have different signal lengths. Will figure out better way to cope with mismatched data but just wanted to see if wavelet transform would be useful. -featureSelect.py - implemented forward search for feature selection

classify.py
@@ -1,70 +1,76 @@
 from featureGen import FeatureGen
+from featureSelect import forward_search
+from featureSelect import cv_accuracy
 from matplotlib import pyplot
 import numpy as np
-from numpy.linalg import inv
-import sklearn
 from sklearn.cross_validation import cross_val_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC, LinearSVC
 
-# Analyze mean force of PD and non-PD subjects
-def analyzeMeans(X, Y):
-	a = zip(X, Y)
-	nonPD = [x[1] for x, y in a if y == 0]
-	PD = [x[1] for x, y in a if y == 1]
-	nonPDmean, PDmean = np.mean(nonPD), np.mean(PD) 
-	nonPDvar, PDvar = np.var(nonPD), np.var(PD)
-	print "Mean: {}, variance: {}".format(nonPDmean, nonPDvar)
-	print "Mean: {}, variance: {}".format(PDmean, PDvar)
+# Analyze mean force for each sensor
+def analyzeSensorMeans(X, Y):
+    print np.mean(X, axis=0)
+
+
+# Analyze mean force for PD and nonPD subjects
+def analyzeGlobalMeans(X, Y):
+    a = zip(X, Y)
+    nonPD = [x[1] for x, y in a if y == 0]
+    PD = [x[1] for x, y in a if y == 1]
+    nonPDmean, PDmean = np.mean(nonPD), np.mean(PD)
+    nonPDvar, PDvar = np.var(nonPD), np.var(PD)
+    print "Mean: {}, variance: {}".format(nonPDmean, nonPDvar)
+    print "Mean: {}, variance: {}".format(PDmean, PDvar)
+
 
 # Run Logistic Regression and plot train and test errors
 def plotTrainTest(clf, X, Y):
-	trainingSizes = range(100, 250, 25)
-	trainErrors, testErrors = [], []
-	for i in trainingSizes:
-		trainX, testX = X[:i], X[i:]
-		trainY, testY = Y[:i], Y[i:]
-		clf.fit(trainX, trainY)
-		
-		# Training error
-		output = clf.predict(trainX)
-		numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, trainY)])
-		numTotal = float(len(trainY))
-		trainErrors.append(numWrong/numTotal)
+    trainingSizes = range(100, 250, 25)
+    trainErrors, testErrors = [], []
+    for i in trainingSizes:
+        trainX, testX = X[:i], X[i:]
+        trainY, testY = Y[:i], Y[i:]
+        clf.fit(trainX, trainY)
 
-		# Test error
-		output = clf.predict(testX)
-		numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, testY)])
-		numTotal = float(len(testY))
-		testErrors.append(numWrong/numTotal)
+        # Training error
+        output = clf.predict(trainX)
+        numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, trainY)])
+        numTotal = float(len(trainY))
+        trainErrors.append(numWrong / numTotal)
 
-	pyplot.plot(trainingSizes, trainErrors, label="Training error")
-	pyplot.plot(trainingSizes, testErrors, label="Test error")
-	pyplot.legend()
-	pyplot.xlabel('Training set size')
-	pyplot.ylabel('Error')
-	pyplot.title('{} train and test error vs. training set size'.format(clf.__class__.__name__))
-	pyplot.show()
+        # Test error
+        output = clf.predict(testX)
+        numWrong = sum([int(predicted != actual) for predicted, actual in zip(output, testY)])
+        numTotal = float(len(testY))
+        testErrors.append(numWrong / numTotal)
+
+    pyplot.plot(trainingSizes, trainErrors, label="Training error")
+    pyplot.plot(trainingSizes, testErrors, label="Test error")
+    pyplot.legend()
+    pyplot.xlabel('Training set size')
+    pyplot.ylabel('Error')
+    pyplot.title('{} train and test error vs. training set size'.format(clf.__class__.__name__))
+    pyplot.show()
+
+
+def crossValidate(clf, X, Y):
+    scores = cross_val_score(clf, X, Y, cv=5)
+    accuracy = sum(scores) / len(scores)
+    print "Test accuracy for {}: {}, nFeatures: {}".format(clf.__class__.__name__, accuracy, len(X[0]))
+    return accuracy
+
 
 def main():
-	# Create feature and label vectors
-	f = FeatureGen()
-	X, Y = f.getXY()
-	analyzeMeans(X, Y)
-	plotTrainTest(LogisticRegression(), X, Y)
+    # Create feature and label vectors
+    f = FeatureGen()
+    X, Y = f.getXY()
 
-main()
+    nF = len(X[0])
+    subsetFeatures = forward_search(list(xrange(nF)), 10, cv_accuracy, [LogisticRegression(), X, Y])
+    print subsetFeatures
+    # analyzeMeans(X, Y)
+    plotTrainTest(LogisticRegression(), np.array(X)[:, subsetFeatures], Y)
+    # crossValidate(LogisticRegression(), X, Y)
 
-# scores = cross_val_score(clf, X, Y, cv=5)
-# print "Test accuracy for Random Forest: {}".format(sum(scores)/len(scores))
 
-# # Simple Unweighted Linear Regression
-# # TODO: it always predicts positive...
-# # Also can we use regression for a classification problem?
-# X, Y = np.matrix(X), np.matrix(Y)
-# theta = inv(X.T * X)*X.T*Y.T
-# output = [1 for pt in X]
-# numCorrect = np.sum((output == Y))
-# numTotal = float(Y.shape[1])
-# print "Accuracy for linear regression: {}".format(numCorrect / numTotal)
+if __name__ == "__main__":
+    main()