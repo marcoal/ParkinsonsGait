22d1fe5d7aeb7be3ed71f8add4b76c75c2223977
Marco <marco93alban@hotmail.com>
2014-12-05 14:02:18 -0800

    featureSelect.py

--- featureSelect.py
+++ featureSelect.py
@@ -1,19 +1,22 @@
 
 import numpy as np
 from sklearn.cross_validation import cross_val_score
+import sys
 
 
 # This code was adapted from Sebastian Raschka
-def forward_search(features, max_k, criterion_func, params):
+def forward_search(nfeatures, max_k, criterion_func, params):
     '''
     Forward search for best subset of features
-    :param features: list of indeces indicating features (i.e. list(xrange(
+    :param features: list of indeces indicating features
     :param max_k:
     :param criterion_func:
     :param params:
     :return:
     '''
 
+
+    features = list(xrange(0, nfeatures))
     clf = params[0]
     X = params[1]
     Y = params[2]
@@ -26,6 +29,7 @@ def forward_search(features, max_k, criterion_func, params):
     if max_k > d:
         max_k = d
 
+    print('Starting Feature select:')
     while True:
         # Evaluate criteriafunc on the subset of parameters
         crit_func_max = criterion_func(feat_sub + [features[0]], clf, X, Y)
@@ -35,6 +39,7 @@ def forward_search(features, max_k, criterion_func, params):
             if crit_func_eval > crit_func_max:
                 crit_func_max = crit_func_eval
                 best_feat = x
+        sys.stdout.write('.')
         feat_sub.append(best_feat)
         features.remove(best_feat)
 
@@ -53,3 +58,10 @@ def cv_accuracy(indexFeatures, clf, X, Y):
     scores = cross_val_score(clf, X[:, indexFeatures], Y, cv=5)
     accuracy = sum(scores) / len(scores)
     return accuracy
+
+def tst_auc(indexFeatures, clf, X, Y):
+    X = np.array(X)
+    Y = np.array(Y)
+    scores = cross_val_score(clf,  X[:, indexFeatures], Y, cv=10, scoring='roc_auc')
+    avgAuc = sum(scores)/float(len(scores))
+    return avgAuc
